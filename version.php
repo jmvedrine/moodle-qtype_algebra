7d8812ac27cace039096c49eb253933f44c10a23
Jean-Michel Vedrine <vedrine@vedrine.org>
2013-05-20 10:19:48 +0200

    CONTRIB-4315 qtype_algebra will not install: Errors found in XMLDB

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_algebra';
-$plugin->version   = 2012062500;
+$plugin->version   = 2012062501;
 
 $plugin->requires  = 2012062500;
-$plugin->release   = '1.1 for Moodle 2.3';
+$plugin->release   = '1.11 for Moodle 2.3';
 $plugin->maturity  = MATURITY_STABLE;
